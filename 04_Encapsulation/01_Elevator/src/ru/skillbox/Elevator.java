1c7191acca66fda325149aaa9b8fa41fecab89bd
Uskov Valerii <uskovvospb@gmail.com>
2021-07-05 00:34:41 +0300

    home task 4.1 is solved.

---
+++ 04_Encapsulation/01_Elevator/src/ru/skillbox/Elevator.java
@@ -0,0 +1,56 @@
+package ru.skillbox;
+
+public class Elevator {
+    private int currentFloor = 1;
+    private int minFloor;
+    private int maxFloor;
+
+    public Elevator(int minFloor, int maxFloor) {
+        this.minFloor = minFloor;
+        this.maxFloor = maxFloor;
+    }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public void moveDown() {
+        currentFloor = currentFloor > minFloor ? currentFloor - 1 : currentFloor;
+    }
+
+    public void moveUp() {
+        currentFloor = currentFloor < maxFloor ? currentFloor + 1 : currentFloor;
+    }
+
+    public void move(int floor) throws InterruptedException {
+        if (floor > getCurrentFloor() && floor <= maxFloor  ) {
+            System.out.println("Лифт идет вверх.");
+            for(int a = getCurrentFloor(); a < floor; a++){
+                Thread.sleep(1000);
+                if (a == 0) {
+//                    a++;
+                    currentFloor = currentFloor + 1;
+                }
+                if (getCurrentFloor() == floor){
+                    break;
+                }
+                System.out.println(getCurrentFloor());
+                moveUp();
+            }
+        }else if (floor < getCurrentFloor() && floor >= minFloor){
+            System.out.println("Лифт идет вниз.");
+            for(int a = getCurrentFloor(); a > floor; a--){
+                Thread.sleep(1000);
+                if (a == 0) {
+//                    a--;
+                    currentFloor = currentFloor -1;
+                }
+                if (getCurrentFloor() == floor){
+                    break;
+                }
+                System.out.println(getCurrentFloor());
+                moveDown();
+            }
+        }
+    }
+}
--- 04_Encapsulation/01_Elevator/src/ru/skillbox/Main.java
+++ 04_Encapsulation/01_Elevator/src/ru/skillbox/Main.java
@@ -1,7 +1,21 @@
 package ru.skillbox;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Elevator elevator = new Elevator(-3, 26);
+
+        while(true) {
+            System.out.println("Лифт на: " + elevator.getCurrentFloor() + " этаже.\n");
+
+            System.out.print("Введите номер этажа: ");
+
+            int floor = new Scanner(System.in).nextInt();
+
+            elevator.move(floor);
+
+        }
     }
 }
