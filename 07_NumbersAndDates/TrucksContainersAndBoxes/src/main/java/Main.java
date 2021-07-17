5f9ba776c6eacab9ed1c8b942d64de906807818d
Uskov Valerii <uskovvospb@gmail.com>
2021-07-17 20:49:20 +0300

    home task 7.7 Task 2 is solved.

--- 07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -1,11 +1,48 @@
+import net.sf.saxon.expr.Component;
+
 import java.util.Scanner;
 
 public class Main {
+    public static final int CONTAINER_MAX = 12;
+    public static final int BOX_MAX = 27;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String boxes = scanner.nextLine();
+        int allBoxes = Integer.parseInt(boxes);
+        int container = 0;
+        int truck = 0;
 
+        if (allBoxes > 0) {
+            truck++;
+            container++;
+            System.out.println("Грузовик: " + getTruck(truck));
+            System.out.println("\tКонтейнер: " + getContainer(container));
+            for (int a = 1; a <= allBoxes; a++) {
+                System.out.println("\t\tЯщик: " + a);
+                if (a == allBoxes) {
+                    break;
+                }
+                if (container % CONTAINER_MAX == 0 && a % BOX_MAX == 0) {
+                    truck++;
+                    System.out.println("Грузовик: " + getTruck(truck));
+
+                }
+                if (a % BOX_MAX == 0) {
+                    container++;
+                    System.out.println("\tКонтейнер: " + getContainer(container));
+                }
+            }
+        }
+        System.out.println("Необходимо:");
+        System.out.println("грузовиков" + " - " + truck + " шт.");
+        System.out.println("контейнеров" + " - " + container + " шт.");
+//            System.out.println("Грузовик: " + truck);
+//            System.out.println("\t Контейнер: " + container);
+//                System.out.println("\t\t Ящик: " + a);
+//            System.out.println("Необходимо:");
+//            System.out.println("грузовиков " + "-" + truck + " шт.");
+//            System.out.println("контейнеров " + "-" + container + " шт.");
         // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
         // пример вывода при вводе 2
         // для отступа используйте табуляцию - \t
@@ -21,4 +58,11 @@ public class Main {
         */
     }
 
+    public static int getContainer(int container) {
+        return container;
+    }
+
+    public static int getTruck(int truck) {
+        return truck;
+    }
 }
