799b1ff7afc6fe06db1e28df98af9931ae2f3b85
Uskov Valerii <uskovvospb@gmail.com>
2021-07-19 00:41:27 +0300

    home task 7.10 Task 1 is solved.

--- 07_NumbersAndDates/Date/src/main/java/Main.java
+++ 07_NumbersAndDates/Date/src/main/java/Main.java
@@ -1,3 +1,7 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Locale;
 
 public class Main {
 
@@ -12,11 +16,20 @@ public class Main {
     }
 
     public static String collectBirthdays(int year, int month, int day) {
+        LocalDate birthday = LocalDate.of(year, month, day);
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy - EEE", Locale.ENGLISH);
+        long yearsAgo = birthday.until(today, ChronoUnit.YEARS);
+        String s = "";
 
+        if (today.compareTo(birthday) >= 0) {
+            for (long a = 0; a <= yearsAgo; a++) {
+                s += a + " - " + formatter.format(birthday.plusYears(a)) + "\n";
+            }
+        }
         //TODO реализуйте метод для построения строки в следующем виде
         //0 - 31.12.1990 - Mon
         //1 - 31.12.1991 - Tue
-        
-        return "";
+        return s;
     }
 }
