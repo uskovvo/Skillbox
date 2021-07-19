63239d4c5824ed37ba1b906c8237c03570b3fb13
Uskov Valerii <uskovvospb@gmail.com>
2021-07-19 23:21:03 +0300

    home task 7.10 Task 2 is solved.

--- 07_NumbersAndDates/Periods/src/main/java/Main.java
+++ 07_NumbersAndDates/Periods/src/main/java/Main.java
@@ -3,11 +3,14 @@ import java.time.LocalDate;
 public class Main {
 
   public static void main(String[] args) {
-
+    LocalDate birthday = LocalDate.of(1995, 05, 23);
+    System.out.println(getPeriodFromBirthday(birthday));
   }
 
   private static String getPeriodFromBirthday(LocalDate birthday) {
-    return "";
+    LocalDate now = LocalDate.now();
+    return birthday.until(now).getYears() + " years " + birthday.until(now).getMonths() + " months " +
+            birthday.until(now).getDays() + " days";
   }
 
 }
