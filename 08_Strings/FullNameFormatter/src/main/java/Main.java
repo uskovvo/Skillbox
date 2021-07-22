7573da6a7e883665eb541303639e028bc427309d
Uskov Valerii <uskovvospb@gmail.com>
2021-07-22 09:45:20 +0300

    home task 8.9 Task 1 and 2

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -10,6 +10,14 @@ public class Main {
       if (input.equals("0")) {
         break;
       }
+
+      String charsetStr = "";
+      int start = input.indexOf(charsetStr) + input.lastIndexOf(" ");
+      for (int a = 0; a < input.length(); a++) {
+        if (a > 'А' && a < 'я' && a != 'Ё' && a !='ё') {
+
+        }
+      }
       //TODO:напишите ваш код тут, результат вывести в консоль.
       //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
     }
--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -1,9 +1,20 @@
+import org.checkerframework.checker.units.qual.C;
+
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
-  }
+        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+        //TODO: напишите ваш код, результат вывести в консоль
+        String encoding = "";
+        int sum = 0;
+        String charset = " ";
+        int start = text.indexOf(charset);
+        int end = text.indexOf("рубл", start);
+        for(int a = start; a < end; a++) {
+            encoding = text.substring(start, end);
+        }
 
+        System.out.println(encoding.trim());
+    }
 }
