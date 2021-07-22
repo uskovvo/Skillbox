f559699f64e57ce27ad29f2bad2ee440473a53fb
Uskov Valerii <uskovvospb@gmail.com>
2021-07-22 22:45:57 +0300

    home task 8.9 Task 1. Try 2

--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -6,15 +6,14 @@ public class Main {
 
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
         //TODO: напишите ваш код, результат вывести в консоль
-        String encoding = "";
         int sum = 0;
-        String charset = " ";
-        int start = text.indexOf(charset);
-        int end = text.indexOf("рубл", start);
-        for(int a = start; a < end; a++) {
-            encoding = text.substring(start, end);
+
+        for(int a = 0; a < text.length(); a++) {
+            int start = text.lastIndexOf(" руб");
+            int end = text.lastIndexOf(" ");
+            sum += Integer.parseInt(text.substring(start, end));
         }
 
-        System.out.println(encoding.trim());
+        System.out.println(sum);
     }
 }
