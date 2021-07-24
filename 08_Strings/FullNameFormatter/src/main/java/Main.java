5fa2495c322c4976eaf6f3783bebb6dc5c121c43
Uskov Valerii <uskovvospb@gmail.com>
2021-07-24 20:48:07 +0300

    home task 8.9 Task 1, 2.

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -2,25 +2,82 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        String s = "";
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-
-      String charsetStr = "";
-      int start = input.indexOf(charsetStr) + input.lastIndexOf(" ");
-      for (int a = 0; a < input.length(); a++) {
-        if (a > 'А' && a < 'я' && a != 'Ё' && a !='ё') {
-
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+            s = input;
+            //TODO:напишите ваш код тут, результат вывести в консоль.
+            //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
         }
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        int count = 0;
+        for (int a = 0; a < s.length(); a++) {
+            if (Character.isSpaceChar(s.charAt(a))) {
+                count++;
+            }
+        }
+        String surname = getSurname(s);
+        String name = getName(s);
+        String patronymic = getPatronymic(s);
+
+        if (surname.isEmpty() || name.isEmpty() || patronymic.isEmpty() || count != 2) {
+            System.out.println("Введенная строка не является ФИО");
+        } else {
+            System.out.println("Фамилия: " + surname);
+            System.out.println("Имя: " + name);
+            System.out.println("Отчество: " + patronymic);
+        }
     }
-  }
+
+    public static String getSurname(String text) {
+        int ind = text.indexOf(" ");
+        if (ind == -1) {
+            return "";
+        }
+        String str = text.substring(0, ind);
+        for (int a = 0; a < str.length(); a++) {
+            if (Character.UnicodeBlock.of(str.charAt(a)).equals(Character.UnicodeBlock.CYRILLIC)) {
+                return str.trim();
+            }
+        }
+        return "";
+    }
+
+    public static String getName(String text) {
+        if (getSurname(text).isEmpty()) {
+            return "";
+        }
+        String next = getSurname(text);
+        int ind = text.indexOf(" ", next.length() + 1);
+        if (ind == -1) {
+            return "";
+        }
+        String str = text.substring(next.length() + 1, ind).trim();
+        for (int a = 0; a < str.length(); a++) {
+            if (Character.UnicodeBlock.of(str.charAt(a)).equals(Character.UnicodeBlock.CYRILLIC)) {
+                return str;
+            }
+        }
+        return "";
+    }
+
+    public static String getPatronymic(String text) {
+        if (getName(text).isEmpty()) {
+            return "";
+        }
+        int ind = text.lastIndexOf(" ", text.length());
+        String str = text.substring(ind, text.length()).trim();
+        for (int a = 0; a < str.length(); a++) {
+            if (Character.UnicodeBlock.of(str.charAt(a)).equals(Character.UnicodeBlock.CYRILLIC)) {
+                return str;
+            }
+        }
+        return "";
+    }
 
 }
--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -6,16 +6,31 @@ public class Main {
 
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
         //TODO: напишите ваш код, результат вывести в консоль
-        int sum = 0;
-        String charset = " руб";
+        int a = summMan(text, "Вася");
+        int b = summMan(text, "Петя");
+        int c = summMan(text, "Маша");
+        int summ = a + b + c;
+        System.out.println(summ);
+    }
 
-        for(int a = 0; a < text.length(); a++) {
-            int start = text.lastIndexOf(charset);
-            int end = text.lastIndexOf(" ", start - 1);
-            String encod = text.substring(end, start);
-            sum += Integer.parseInt(encod.trim());
+    public static int summMan(String str, String name) {
+        int earn = 0;
+        int i;
+
+        for (int a = 0; a < str.length(); a++) {
+            i = str.indexOf(name); //определяем индекс переданной строки name
+            if (i == -1) break; //если не нашли переданную переменную, возвращаемся в начало
+            str = str.substring(i + name.length()).trim(); //получаем строку после определенного индекса и его
+            //длины, убираем лишние пробелы спереди и сзади
+            i = str.indexOf(" руб"); //находим индекс по переданной строке
+            if (i == -1) break; //если не нашли переданную переменную, возвращаемся в начало
+            str = str.substring(0, i).trim(); //получаем строку от нулевого индекса и до найденного индекса,
+            //убираем пробелы с двух сторон
+            i = str.lastIndexOf(" "); //находим индекс по переданной строке с конца найденной строки
+            if (i == -1) break; //если не нашли переданную переменную, возвращаемся в начало
+            earn = Integer.parseInt(str.substring(i, str.length()).trim()); //получаем число через преобразование
+            //строки в число
         }
-
-        System.out.println(sum);
+        return earn; //возвращаем найденное число
     }
 }
