8adf25e1eaa1a711f7e28fa83803fabb602716da
Uskov Valerii <uskovvospb@gmail.com>
2021-07-25 01:09:53 +0300

    home task 8.9 Task 3 is solved.

--- 08_Strings/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,12 +1,38 @@
+import net.sf.saxon.expr.Component;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        String text = "Коля заработал 50000 рублей, Федя - 45800 рубля, а Саша - 23000 рублей";
+        System.out.println(calculateSalarySum(text));
 
-  }
+    }
 
-  public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
-  }
+    public static int calculateSalarySum(String text) {
+        String alphabet = "[^";
+        for (char a = 'А'; a <= 'я'; a++) {
+            alphabet += a;
+        }
+        alphabet = alphabet + 'ё' + 'Ё' + "]";
+        int summ = 0;
+        String regex = alphabet + "[^,.:;\\-\\s][0-9]*[^,.:;\\-\\s]" + alphabet;
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            int start = matcher.start();
+            int end = matcher.end();
+            for(int a = 0; a < text.substring(start, end).trim().length(); a++){
+                if (!Character.isDigit(text.substring(start, end).trim().charAt(a))) {
+                    return 0;
+                }
+            }
+            summ += Integer.parseInt(text.substring(start, end).trim());
+        }
+        return summ;
+    }
 
 }
