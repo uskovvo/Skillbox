bad27aea57050efb40846bd8e7271415ee64b449
Uskov Valerii <uskovvospb@gmail.com>
2021-07-26 21:01:10 +0300

    home task 8.9 Task 4 is solved.

--- 08_Strings/SplitText/src/main/java/Main.java
+++ 08_Strings/SplitText/src/main/java/Main.java
@@ -1,12 +1,24 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-  }
+    }
 
-  public static String splitTextIntoWords(String text) {
-    //TODO реализуйте метод
-    return "";
-  }
-
+    public static String splitTextIntoWords(String text) {
+        if (!text.isEmpty()) {
+            String encod = "";
+            StringBuilder stringBuilder = new StringBuilder();
+            String regex = "[,.:;\\-0-9]";
+            encod = text.replaceAll(regex, " ");
+            String[] word = encod.split("[\\s]+");
+            for (int a = 0; a < word.length; a++) {
+                stringBuilder.append(word[a]).append("\n");
+            }
+            return stringBuilder.toString().trim();
+        }
+        return "";
+    }
 }
