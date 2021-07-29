ddcacbf7ac703582edcc5c5282e1278e667ca148
Uskov Valerii <uskovvospb@gmail.com>
2021-07-29 22:39:57 +0300

    home task 9.1 Task 1 is solved.

--- 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/Main.java
@@ -1,6 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         String line = "Каждый охотник желает знать, где сидит фазан";
+        String[] strings = line.split(",?\\s+");
+
+        System.out.println(Arrays.toString(ReverseArray.reverse(strings)));
     }
 }
--- 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/ReverseArray.java
+++ 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/ReverseArray.java
@@ -1,7 +1,12 @@
 public class ReverseArray {
 
     //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
-    public static String[] reverse (String[] strings){
+    public static String[] reverse(String[] strings) {
+        for (int a = 0; a < strings.length / 2; a++) {
+            String element = strings[a];
+            strings[a] = strings[strings.length - 1 - a];
+            strings[strings.length - 1 - a] = element;
+        }
         return strings;
     }
 }
