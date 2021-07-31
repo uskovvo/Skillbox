0d31503f6005882df7fa3e60b17dc162f9eea3ed
Uskov Valerii <uskovvospb@gmail.com>
2021-07-31 12:42:10 +0300

    home task 9.1 Task 2 is solved.

--- 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
+++ 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
@@ -1,22 +1,47 @@
+import java.text.DecimalFormat;
+import java.util.Arrays;
+
 public class Hospital {
 
     public static float[] generatePatientsTemperatures(int patientsCount) {
-
+        float[] temperature = new float[patientsCount];
+        for (int a = 0; a < patientsCount; a++) {
+            temperature[a] = getTemperature(32, 40);
+        }
         //TODO: напишите метод генерации массива температур пациентов
+        return temperature;
+    }
 
-        return new float[0];
+    public static float getTemperature(float minTemperature, float maxTemperature) {
+        float temperature = (float) ((Math.random() * (maxTemperature - minTemperature)) + minTemperature);
+        return temperature;
     }
 
     public static String getReport(float[] temperatureData) {
+        float totalTemperature = 0;
+        float middleTemperature;
+        int countHealthyPatient = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int a = 0; a < temperatureData.length; a++) {
+            totalTemperature += temperatureData[a];
+            stringBuilder.append(temperatureData[a]).append(" ");
+            if (temperatureData[a] < 37.0 && temperatureData[a] > 36.1) {
+                countHealthyPatient++;
+            }
+        }
+
+        middleTemperature = totalTemperature / temperatureData.length;
+        double scale = Math.pow(10, 2);
+        middleTemperature = (float) (Math.round(middleTemperature * scale) / scale);
+
         /*
         TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
             а также температуры всех пациентов.
         */
-
         String report =
-                "Температуры пациентов: " + 0 +
-                        "\nСредняя температура: " + 0 +
-                        "\nКоличество здоровых: " + 0;
+                "Температуры пациентов: " + stringBuilder.toString().trim() +
+                        "\nСредняя температура: " + middleTemperature +
+                        "\nКоличество здоровых: " + countHealthyPatient;
 
         return report;
     }
