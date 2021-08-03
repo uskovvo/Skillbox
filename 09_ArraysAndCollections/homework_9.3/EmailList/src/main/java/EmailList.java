d08ca7a323a16c236bb0ad32ff43caf7013c7777
Uskov Valerii <uskovvospb@gmail.com>
2021-08-03 23:14:26 +0300

    home task 9.3 is solved.

--- 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
@@ -1,15 +1,33 @@
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EmailList {
+    TreeSet<String> treeSet = new TreeSet<>();
+    Pattern pattern;
+    Matcher matcher;
+    String regex = "^[a-z]+[a-z0-9._-]+@" + "[a-z]+[a-z0-9]\\.[a-z]{2,3}";
 
     public void add(String email) {
+        email = email.toLowerCase(Locale.ROOT);
+        pattern = Pattern.compile(regex);
+        if(validate(email)) {
+            treeSet.add(email);
+        }else {
+            System.out.println("Неверный формат email");
+        }
         // TODO: валидный формат email добавляется
     }
 
     public List<String> getSortedEmails() {
         // TODO: возвращается список электронных адресов в алфавитном порядке
-        return Collections.emptyList();
+        return new ArrayList<>(treeSet);
+    }
+
+    public boolean validate(String email) {
+        matcher = pattern.matcher(email);
+
+        return matcher.matches();
     }
 
 }
--- 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
@@ -1,8 +1,9 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
-    
+    private static final EmailList emailList = new EmailList();
     /* TODO:
         Пример вывода списка Email, после ввода команды LIST в консоль:
         test@test.com
@@ -19,15 +20,48 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("0")) {
                 break;
             }
-            
+
+            String command = findCommand(input);
+            String email = emailText(input);
+            if (command.equals("ADD")) {
+                emailList.add(email);
+            }
+            if (command.equals("LIST")) {
+                emailList.getSortedEmails();
+            }
             //TODO: write code here
-            
+
         }
+    }
+
+    public static String findCommand(String request) {
+        String command;
+        String regex = "[\\W+0-9]";
+        request = request.replaceAll(regex, " ").trim();
+        int start = request.indexOf("");
+        int end = request.indexOf(" ");
+        if (end == -1) {
+            command = request.substring(start);
+        } else {
+            command = request.substring(start, end);
+        }
+        command = command.toUpperCase(Locale.ROOT);
+
+        return command;
+    }
+
+    public static String emailText(String request) {
+        int start = request.indexOf(" ");
+        if (start == -1) {
+            return "";
+        }
+
+        return request.substring(start).trim();
     }
 }
