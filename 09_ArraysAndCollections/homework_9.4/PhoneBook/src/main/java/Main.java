06e3f4afe4198c73e2e064d913765db7606a4c88
Uskov Valerii <uskovvospb@gmail.com>
2021-08-05 23:25:44 +0300

    home task 9.4

--- 09_ArraysAndCollections/homework_9.4/PhoneBook/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.4/PhoneBook/src/main/java/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+        String phone = "";
 
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+
+            if(input.equals("LIST")) {
+                for(String phones : phoneBook.getAllContacts()){
+                    System.out.println(phones);
+                }
+            }
+
+            if (!input.isEmpty() && Character.isDigit(input.charAt(0))) {
+                phone = input;
+                name = scanner.nextLine();
+            }else {
+                name = input;
+                phone = scanner.nextLine();
+            }
+            phoneBook.addContact(phone, name);
+
+        }
     }
 }
--- 09_ArraysAndCollections/homework_9.4/PhoneBook/src/main/java/PhoneBook.java
+++ 09_ArraysAndCollections/homework_9.4/PhoneBook/src/main/java/PhoneBook.java
@@ -1,29 +1,92 @@
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PhoneBook {
+    TreeMap<String, String> phoneBook = new TreeMap<>();
+    Pattern patternName;
+    Pattern patternNumber;
+    Matcher matcherName;
+    Matcher matcherNumber;
+    String regexName = "[А-Я][а-я]+";
+    String regexNumber = "[78][0-9]{10}";
 
     public void addContact(String phone, String name) {
+        patternName = Pattern.compile(regexName);
+        patternNumber = Pattern.compile(regexNumber);
+        if (!validateName(phone) || !validateNumber(name)) {
+            return;
+        } else {
+            phoneBook.put(phone, name);
+        }
         // проверьте корректность формата имени и телефона
         // если такой номер уже есть в списке, то перезаписать имя абонента
     }
 
     public String getNameByPhone(String phone) {
+        if (!validateNumber(phone)) {
+            return "";
+        }
+        for (String key : phoneBook.keySet()) {
+            if (phoneBook.containsKey(phone)) {
+                return phoneBook.get(key) + " - " + key;
+            }
+        }
+        System.out.println("Такого номера нет в телефонной книге.");
+
         // формат одного контакта "Имя - Телефон"
         // если контакт не найдены - вернуть пустую строку
         return "";
     }
 
     public Set<String> getPhonesByName(String name) {
+        TreeSet<String> contactsByName = new TreeSet<>();
+        if (!validateName(name)) {
+            return contactsByName;
+        }
+        for (String key : phoneBook.keySet()) {
+            if (phoneBook.get(key).equals(name)) {
+                contactsByName.add(name + " - " + key);
+            }
+        }
+        if (contactsByName.isEmpty()) {
+            System.out.println("Такого имени в телефонной книге нет.");
+        }
         // формат одного контакта "Имя - Телефон"
         // если контакт не найден - вернуть пустой TreeSet
-        return new TreeSet<>();
+        return contactsByName;
     }
 
     public Set<String> getAllContacts() {
+        TreeSet<String> contacts = new TreeSet<>();
+        String phone = "";
+        String name = "";
+        for (String key : phoneBook.keySet()) {
+            name = phoneBook.get(key);
+            phone = String.join(", ", getPhonesByName(name));
+            phone = phone.replaceAll(name + " - ", "");
+            contacts.add(name + " - " + phone);
+        }
         // формат одного контакта "Имя - Телефон"
         // если контактов нет в телефонной книге - вернуть пустой TreeSet
-        return new TreeSet<>();
+        return contacts;
     }
 
+    public boolean validateName(String name) {
+        matcherName = patternName.matcher(name);
+        if (!matcherName.find()) {
+            System.out.println("Неверный формат ввода");
+            return false;
+        }
+        return matcherName.matches();
+    }
+
+    public boolean validateNumber(String phone) {
+        matcherNumber = patternNumber.matcher(phone);
+        if (!matcherNumber.find()) {
+            System.out.println("Неверный формат ввода");
+            return false;
+        }
+        return matcherNumber.matches();
+    }
 }
