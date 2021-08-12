5675416fee056dc8dc023256b92f55ff5d97deac
Uskov Valerii <uskovvospb@gmail.com>
2021-08-12 23:02:34 +0300

    home task 10.2 modifiers

--- 10_InheritanceAndPolymorphism/homework_10.1/src/main/java/Main.java
+++ 10_InheritanceAndPolymorphism/homework_10.1/src/main/java/Main.java
@@ -1,10 +1,8 @@
 import java.time.LocalDate;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         BankAccount bankAccount = new BankAccount();
-        BankAccount bankAccount1 = new BankAccount();
         BankAccount cardAccount = new CardAccount();
         DepositAccount depositAccount = new DepositAccount();
         bankAccount.put(500);
