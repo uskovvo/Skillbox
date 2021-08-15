08eca1824fd8dd8131ddfcb3f5aa9f2b8eb37e6e
Uskov Valerii <uskovvospb@gmail.com>
2021-08-15 22:20:40 +0300

    home task 10.3 corrected.

--- 10_InheritanceAndPolymorphism/homework_10.3/src/main/java/IndividualBusinessman.java
+++ 10_InheritanceAndPolymorphism/homework_10.3/src/main/java/IndividualBusinessman.java
@@ -1,6 +1,10 @@
 public class IndividualBusinessman extends Client {
+    private final double PERCENT_MORE_THOUSAND = 0.005;
+    private final double PERCENT_LESS_THOUSAND = 0.01;
+
     @Override
     public void put(double amountToPut) {
-        super.put(amountToPut < 1000 ? amountToPut - (amountToPut * 0.01) : amountToPut - (amountToPut * 0.005));
+        super.put(amountToPut < 1000 ? amountToPut - (amountToPut * PERCENT_LESS_THOUSAND) :
+                                        amountToPut - (amountToPut * PERCENT_MORE_THOUSAND));
     }
 }
