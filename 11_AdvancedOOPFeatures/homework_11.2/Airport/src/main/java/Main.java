29bd7e6f18d3f95a2a28c8814dea9a2a247ff39f
Uskov Valerii <uskovvospb@gmail.com>
2021-09-11 13:25:18 +0300

    home task 11.2.2 solved

--- 11_AdvancedOOPFeatures/homework_11.2/Airport/src/main/java/Main.java
+++ 11_AdvancedOOPFeatures/homework_11.2/Airport/src/main/java/Main.java
@@ -2,22 +2,24 @@ import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
 import com.skillbox.airport.Terminal;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collections;
+import java.time.LocalTime;
+import java.time.ZoneId;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
 
-
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        airport = Airport.getInstance();
-        airport.getTerminals().stream().map(Terminal::getFlights);
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        return airport.getTerminals().stream()
+                .flatMap(terminal -> terminal.getFlights().stream()
+                        .filter(flight -> flight.getType() == Flight.Type.DEPARTURE &&
+                                LocalTime.ofInstant(flight.getDate().toInstant(), ZoneId.systemDefault())
+                                .isAfter(LocalTime.now()) &&
+                                LocalTime.ofInstant(flight.getDate().toInstant(), ZoneId.systemDefault())
+                                .isBefore(LocalTime.now().plusHours(2)))).collect(Collectors.toList());
     }
 
 }
