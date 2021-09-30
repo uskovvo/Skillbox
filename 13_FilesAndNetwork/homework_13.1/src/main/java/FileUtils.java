fba5aab8f96f8ac9813ba8baaba85dc25b7b6dfd
Uskov Valerii <uskovvospb@gmail.com>
2021-09-30 22:21:30 +0300

    home task 13.1 is solved.

--- 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
@@ -1,6 +1,43 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FileUtils {
+    private static long pathSize;
+    private static long size;
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        File folder = new File(path);
+        File[] files = folder.listFiles();
+
+        try {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    pathSize += getFileSize(file);
+                    size = 0;
+                } else {
+                    pathSize += file.length();
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return pathSize;
+    }
+
+    public static long getFileSize(File file) {
+        if (file.isDirectory()) {
+            ArrayList<File> path = new ArrayList<>(Arrays.asList(file.listFiles()));
+            for (File f : path) {
+                if (f.isDirectory()) {
+                    getFileSize(f);
+                } else {
+                    size += f.length();
+                }
+            }
+        } else {
+            size += file.length();
+        }
+        return size;
     }
 }
--- 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
@@ -1,5 +1,40 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    private static long size;
+    private static String path;
+    private static Scanner scanner;
+    private static final long MAX_SIZE = 1_024L;
+    private static String unit = "byte";
+
+    public static void main(String[] args) {
+        try {
+            for (; ; ) {
+                System.out.println("Введите путь к папке: ");
+                scanner = new Scanner(System.in);
+                path = scanner.nextLine();
+                System.out.println("Пользователь ввёл: " + path);
+                size = FileUtils.calculateFolderSize(path);
+                System.out.println(toString(size));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static String toString(long size) {
+        if (size > MAX_SIZE) {
+            size /= MAX_SIZE;
+            unit = "Kb";
+        }
+        if (size > MAX_SIZE) {
+            size /= MAX_SIZE;
+            unit = "Mb";
+        }
+        if (size > MAX_SIZE) {
+            size /= MAX_SIZE;
+            unit = "Gb";
+        }
+        return "Размер выбранной папки: " + size + unit + "\n";
     }
 }
