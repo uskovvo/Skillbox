930befd29d9e7ce812bed6533329852169e47467
Uskov Valerii <uskovvospb@gmail.com>
2021-10-04 22:05:20 +0300

    Home Task 13.2 ver.2

--- 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
@@ -9,17 +9,33 @@ public class FileUtils {
     private static Path destination;
 
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
+        source = Paths.get(sourceDirectory);
+        destination = Paths.get(destinationDirectory);
         try {
-            List<Path> files = Files
-                    .walk(Paths.get(sourceDirectory))
-                    .collect(Collectors.toList());
-            List<Path> destination = files.stream()
-                    .map(Paths.get(sourceDirectory)::relativize)
-                    .map(Paths.get(destinationDirectory)::resolve)
-                    .collect(Collectors.toList());
-            for (int a = 1; a < files.size(); a++) {
-                Files.copy(files.get(a), destination.get(a));
+            if (!Files.exists(destination)) {
+                Files.createDirectory(destination);
             }
+
+            Files.walkFileTree(source, new SimpleFileVisitor<Path>() {
+
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    Path copy = destination.resolve(source.relativize(dir));
+                    if (!Files.exists(copy)) {
+                        Files.createDirectory(copy);
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Path copy = destination.resolve(source.relativize(file));
+                    if (!Files.exists(copy)) {
+                        Files.copy(file, copy);
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
         } catch (Exception ex) {
             ex.printStackTrace();
         }
