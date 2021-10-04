ded8ffac8534dc69157f64b180698999f9b722cd
Uskov Valerii <uskovvospb@gmail.com>
2021-10-04 22:03:53 +0300

    Home Task 13.2 ver.1

--- 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
@@ -1,5 +1,28 @@
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FileUtils {
+    private static Path source;
+    private static Path destination;
+
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
+        try {
+            List<Path> files = Files
+                    .walk(Paths.get(sourceDirectory))
+                    .collect(Collectors.toList());
+            List<Path> destination = files.stream()
+                    .map(Paths.get(sourceDirectory)::relativize)
+                    .map(Paths.get(destinationDirectory)::resolve)
+                    .collect(Collectors.toList());
+            for (int a = 1; a < files.size(); a++) {
+                Files.copy(files.get(a), destination.get(a));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
         // TODO: write code copy content of sourceDirectory to destinationDirectory
     }
 }
--- 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
+    private static String sourcePath;
+    private static String destinationPath;
+    private static Scanner scanner;
+
     public static void main(String[] args) {
+
+        try {
+            for (; ; ) {
+                System.out.println("Введите путь копируемой папке.");
+                scanner = new Scanner(System.in);
+                sourcePath = scanner.nextLine();
+                System.out.println("Введите путь куда копировать.");
+                destinationPath = scanner.nextLine();
+                FileUtils.copyFolder(sourcePath, destinationPath);
+
+                System.out.println("Копируем папку: " + sourcePath + "\n" +
+                        "В папку: " + destinationPath + "\n");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
