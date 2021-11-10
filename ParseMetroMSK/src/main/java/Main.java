e014d8021a298d330e2d8c30cc4d02608d2d1dfa
Uskov Valerii <uskovvospb@gmail.com>
2021-11-10 22:11:57 +0300

    Home Task 13.5. Problem.

--- ParseMetroMSK/src/main/java/Main.java
+++ ParseMetroMSK/src/main/java/Main.java
@@ -1,15 +1,17 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Main {
-    private static final String WEB_SITE = "https://www.moscowmap.ru/metro.html#lines";
+    private static final String WEB_SITE = "https://infotables.ru/strany-i-goroda/1066-metro-moskvy-spisok-stantsij";
 
     public static void main(String[] args) {
         try{
             Document doc = Jsoup.connect(WEB_SITE).maxBodySize(0).get();
-            Elements elements = doc.select("div.js-toggle-depend");
-            SubwayMSK.createSubwayMap(elements);
+            Elements elements = doc.select("div.item-page");
+            FullSubwayMSK.createSubwayMap(elements);
+//            SubwayMSK.createSubwayMap(elements);
         }
         catch (Exception ex){
             ex.printStackTrace();
