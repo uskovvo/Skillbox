73daa9eca87d013234d92d1e39cfdb2501793c9b
Валерий Олегович <uskovvospb@gmail.com>
2021-11-25 19:52:34 +0000

    Update Main.java

--- 14_SQLAndHibernate/JDBCExp/src/main/java/Main.java
+++ 14_SQLAndHibernate/JDBCExp/src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/skillbox";
         String user = "root";
-        String pass = "eC9955956682";
+        String pass = "TestTest";
 
         try {
             Connection connection = DriverManager.getConnection(url, user, pass);
