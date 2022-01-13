391dbcad8fa6ffef8aad49afb984c5b0483db5d8
uskovvo <uskovvospb@gmail.com>
2022-01-13 19:23:18 +0300

    16.3 last

--- 15_Multithreading/SiteLinks/src/main/java/Links.java
+++ 15_Multithreading/SiteLinks/src/main/java/Links.java
@@ -6,6 +6,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -20,40 +22,47 @@ public class Links extends RecursiveAction {
     private final String mainLink;
     private final Set<Links> links = new LinkedHashSet<>();
     private PrintWriter printWriter;
+    private String exceptionLink;
 
-    public Links (String mainLink){
+    public Links(String mainLink) {
         this.mainLink = mainLink;
     }
 
     @Override
     protected void compute() {
-        try{
+        try {
             printWriter = new PrintWriter("links.txt");
-            Document document = Jsoup.connect(mainLink).ignoreContentType(true).get();
-            Elements elements = document.select("a");
-            elements.forEach(element -> {
-                String newLink = element.absUrl("href");
-                Pattern pattern = Pattern.compile(mainLink);
-                Matcher matcher = pattern.matcher(newLink);
-                if(matcher.find() && !links.contains(newLink)){
-//                    Links link = new Links(newLink);
-                    printWriter.write(newLink + "\n\t");
-                    links.add(new Links(newLink));
+        } catch (FileNotFoundException e) {
+            logger.fatal(marker, "Fatal read file", e);
+        }
+        Document document = null;
+        try {
+            document = Jsoup.connect(mainLink).ignoreContentType(true).get();
+        } catch (IOException e) {
+            logger.fatal(marker, "Fatal read main link: ", e);
+        }
 
-                    try {
-                        Thread.sleep(125);
-                    } catch (InterruptedException e) {
-                        logger.error(marker, "Error sleep: ", e);
-                        logger.fatal(marker, "Fatal sleep: ", e);
-                    }
+        assert document != null;
+        Elements elements = document.select("a");
+        elements.forEach(element -> {
+            String newLink = element.absUrl("href");
+            Pattern pattern = Pattern.compile(mainLink);
+            Matcher matcher = pattern.matcher(newLink);
+            if (matcher.find() && !links.contains(newLink)) {
+//                    Links link = new Links(newLink);
+                printWriter.write(newLink + "\n\t");
+                links.add(new Links(newLink));
+
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    logger.error(marker, "Error sleep: ", e);
+                    logger.fatal(marker, "Fatal sleep: ", e);
                 }
-            });
+
+            }
+        });
 //            links.forEach(Links::join);
-        }
-        catch (Exception e){
-            logger.error(marker, "Error: ", e);
-            logger.fatal(marker, "Fatal error: ", e);
-        }
 
         printWriter.flush();
         printWriter.close();
--- 15_Multithreading/SiteLinks/src/main/java/Main.java
+++ 15_Multithreading/SiteLinks/src/main/java/Main.java
@@ -2,6 +2,8 @@ import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
-        new ForkJoinPool().invoke(new Links("https://lenta.ru/"));
+        new ForkJoinPool().invoke(new Links("https://skillbox.ru/"));
+//        Links link = new Links("https://lenta.ru/");
+//        link.compute();
     }
 }
