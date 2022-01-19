4926ae4328fffc073af9f16d4796d8be2699683d
uskovvo <uskovvospb@gmail.com>
2022-01-19 23:12:51 +0300

    Home task 19.4 is done.

--- SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
+++ SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
@@ -1,14 +1,34 @@
 package main;
 
+import main.model.Installation;
+import main.model.InstallationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Date;
 
-@RestController
+@Controller
 public class DefaultController {
+
+    @Autowired
+    InstallationRepository installationRepository;
+
+
     @RequestMapping("/")
-    public String index(){
-        return (new Date()).toString();
+    public String index(Model model){
+        Iterable<Installation> installationIterable = installationRepository.findAll();
+        ArrayList<Installation> installs = new ArrayList<>();
+        for(Installation installation: installationIterable){
+            installs.add(installation);
+        }
+
+        model.addAttribute("installs", installs);
+        model.addAttribute("installsCount", installs.size());
+        return "index";
     }
 }
